68b17678f64f1b8d5f0e1b57c68981b8c48e6a68
diasvanessa <ferreirad.vanessa@gmail.com>
2025-12-26 17:56:27 -0300

    commit inicial

---
+++ src/01-intro/aula01.py
@@ -0,0 +1,15 @@
+""" aula01 - Introdução ao Python
+"""
+
+print('olá mundo!!!! ')
+
+
+def somar(numero_01, numero_02):
+    """ Função que soma dois números """
+    return numero_01 + numero_02
+
+
+SOMA = somar(10.3, 2.5)
+print(
+    SOMA
+)
