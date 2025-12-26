70ddbc3475768faf27b9205b44d9a67c756d8e2e
diasvanessa <ferreirad.vanessa@gmail.com>
2025-12-26 18:35:19 -0300

    aula 03 - comentarios

---
+++ src/01-intro/aula03.py
@@ -0,0 +1,18 @@
+""" Aula 03 - Comentarios """
+
+# Comentario de linha unica - single line comment
+
+''' 
+Comentario de
+multiplas linhas - multi line comment
+'''
+
+# atribui o valor 10 a variavel n1
+n1 = 10
+
+n2 = 30
+n3 = 78
+
+print(n1, n2)
+print(n3)
+
