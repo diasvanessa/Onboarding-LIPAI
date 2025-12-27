eb3d843efecdf27856afc01144ea2241e2afda2b
diasvanessa <ferreirad.vanessa@gmail.com>
2025-12-27 14:27:00 -0300

    Aula 02.04 - while

---
+++ src/02-controle_de_fluxo/aula04.py
@@ -0,0 +1,17 @@
+""" Aula 02.04 - while """
+
+# while condicao:
+#    instrucao
+#    instrucao
+
+contador = 0
+while contador <= 10:
+    print(contador)
+    contador += 1
+    
+print("fim!")
+
+# linha = arquivo.readLine()
+# while linha:
+#     print(linha)
+#     linha = arquivo.readLine()
