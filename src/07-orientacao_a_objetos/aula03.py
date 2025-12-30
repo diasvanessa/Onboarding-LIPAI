74f25d4e255d493fba0e73c858ba13017134173b
diasvanessa <ferreirad.vanessa@gmail.com>
2025-12-30 13:45:26 -0300

    Aula 07.03 – Métodos de classe

---
+++ src/07-orientacao_a_objetos/aula03.py
@@ -0,0 +1,41 @@
+""" Aula 07.03 – Métodos de classe """
+
+class Retangulo: 
+    def __init__(self, base, altura):
+        self.base = base
+        self.altura = altura
+    
+    # metodos associados a instancia
+    def calcular_area(self):
+        return self.base * self.altura
+    
+    def calcular_perimetro(self):
+        return 2 * (self.base + self.altura)
+    
+    @classmethod # metodo de classe e nao de instancia
+    def from_list(cls, lista):
+        return cls(lista[0], lista[1])
+    
+    @classmethod
+    def from_string(cls, rep_retangulo):
+        base, altura = rep_retangulo.split(sep=',')
+        return cls(float(base), float(altura))
+    
+retangulo1 = Retangulo(10.0, 5.0)
+retangulo2 = Retangulo(6.0, 3.0)
+breakpoint()
+retangulo3 = Retangulo.from_list([20.0, 3.5])
+retangulo4 = Retangulo.from_string('55.4,13.5')
+
+print(retangulo1.calcular_area())
+print(retangulo2.calcular_area())
+
+print(retangulo3.base, 
+      retangulo3.altura, 
+      retangulo3.calcular_area(), 
+      retangulo3.calcular_perimetro())
+
+print(retangulo4.base, 
+      retangulo4.altura, 
+      retangulo4.calcular_area(), 
+      retangulo4.calcular_perimetro())
